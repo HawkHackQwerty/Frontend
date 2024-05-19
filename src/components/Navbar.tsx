52b89fd1a9dff95847b88e6da120540727b3f8cb
Krish Chopra <krish.chopra23@gmail.com>
2024-05-18 23:35:00 -0400

    fixed title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ return (
     <>
     <ChakraProvider>
     <div style={{ boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.2)", display: "flex", alignItems: "center", width: "100vw", height: "9vh", background: "linear-gradient(to bottom, #87CEEB, #ADD8E6)" }}>
-        <Heading style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100vw", height: "100vh" }} marginLeft={ "15px" }>Mesh-Mesh</Heading>
+        <Heading style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100vw", height: "100vh" }} marginLeft={ "15px" }>AImploy</Heading>
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "9vw" }} >
             <Avatar src={funny} style={{ boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)" }} />
             <Heading size="xs" style={{ marginTop: "4px", marginLeft: "auto", marginRight: "auto" }}>Fahmi Omer</Heading>
